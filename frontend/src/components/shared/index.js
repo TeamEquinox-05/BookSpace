11883ac78a0ade964be40eb2525c3c18a32e42fc
tanmaydesai07 <tanmay2004desai@gmail.com>
2025-08-18 23:05:02 +0530

    fixed wrong import

--- frontend/src/components/shared/index.js
+++ frontend/src/components/shared/index.js
@@ -3,3 +3,5 @@ export { default as RecentBookings } from './RecentBookings';
 export { default as PageHeader } from './PageHeader';
 export { default as PopularPlacesList } from './PopularPlacesList';
 export { default as ForgotPasswordModal } from './ForgotPasswordModal';
+export { default as BookingModal } from './BookingModal';
+export { default as ConfirmationModal } from './ConfirmationModal';
