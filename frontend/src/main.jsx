df182d702ace6143c8b879b25f78f8fe9267d6bc
TeamEquinox-05 <teamequinox05@gmail.com>
2025-09-17 13:05:13 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
-axios.defaults.baseURL = 'https://bookspace-pflj.onrender.com/api';
+axios.defaults.baseURL = 'https://bookspace-pflj.onrender.com';
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
