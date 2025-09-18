accbe14de9cba42db0234eae3f0f0d6e51428c73
RhugvedDangui <rhugveddangui07@gmail.com>
2025-09-18 13:28:22 +0530

    port changed

--- backend/server.cjs
+++ backend/server.cjs
@@ -43,7 +43,7 @@ app.use('/api/bookings', require('./src/routes/bookings.cjs'));
 app.use('/api/stats', require('./src/routes/stats.cjs'));
 app.use('/api/users', require('./src/routes/users.cjs'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
